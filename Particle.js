33a4a0b1a42ae87331d04fc2d7613a366ff0f9a0
MayankShaw <80954305+MayankShaw@users.noreply.github.com>
2022-03-15 20:48:43 +0530

    Add files via upload

--- Particle.js
+++ Particle.js
@@ -9,6 +9,7 @@ class Particles {
         //give color property 
         this.color = color(random(0,255),random(0,255),random(0,255))
         World.add(world, this.body);
+      
     }
     display() {
 
@@ -20,7 +21,7 @@ class Particles {
         rotate(angle);
         imageMode(CENTER);
         noStroke();
-        fill("white");
+        fill(this.color);
         ellipseMode(RADIUS);
         ellipse(0,0,this.r,this.r);
         pop();
